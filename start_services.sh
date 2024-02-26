d95e46da9c1e6d57ce66d021b1074b1269c29d84
Zack J <zackj@poweredbyshard.com>
2024-02-26 07:41:58 -0800

    Update start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
+# Start the Celery worker in the background
+celery -A main.celery worker --loglevel=info &
 # Start the Flask app in the background
-gunicorn main:app &
-
-# Start the Celery worker
-celery -A main.celery worker --loglevel=info
+gunicorn main:app
